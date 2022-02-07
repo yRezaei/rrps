7b1365f0c4c9f29f00c77fbaffa778c9c3a390c3
Yashar A. Rezaei <yashar.a.rezaei@gmail.com>
2022-02-08 00:01:11 +0100

    rrps build as a library and test case added

--- test/tester.cpp
+++ test/tester.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "rrps.hpp"
+#include "../rrps/include/rrps.hpp"
 #include <memory>
 #include <thread>
 #include <chrono>
@@ -34,17 +34,15 @@ struct Com1 : public rrps::IServiceProvider<BaseObj, BaseObj>
 private:
     std::string id{"com1"};
     BaseObj arg{1, 5.5};
-    std::unordered_map<std::string, rrps::SubCallback<BaseObj> > sub_callback_list;
+    std::unordered_map<std::string, rrps::SubCallback<BaseObj>> sub_callback_list;
     RandInt randInt{0, 3};
     std::mutex arg_mutex;
 
 private:
-
-
 public:
     void operator()()
     {
-        //std::this_thread::sleep_for(std::chrono::seconds(randInt()));
+        // std::this_thread::sleep_for(std::chrono::seconds(randInt()));
         arg.a++;
         arg.d++;
         for (auto &sub : sub_callback_list)
@@ -56,17 +54,17 @@ public:
     void submit_publish_and_response_services(std::shared_ptr<rrps::IProvideServiceRegistration<BaseObj, BaseObj>> provide_registrator)
     {
         provide_registrator->add_publish_service_register_callback("com1_arg_updated", [&](const std::string &subscriber_id, std::function<void(BaseObj)> sub_callback)
-        {
+                                                                   {
             if (sub_callback) {
                 sub_callback_list[subscriber_id] = sub_callback;
             }
             else {
                 std::cout << "ERROR: subscriber_id want to subscribe with empty function!!!" << std::endl;
-            }
-        });
+            } });
 
-        provide_registrator->add_response_service_register_callback("com1_get_arg", [&](const std::string &requester_id) { return [&](BaseObj a) 
-                                                                                        { const std::lock_guard<std::mutex> lock(arg_mutex); return arg; }; });
+        provide_registrator->add_response_service_register_callback("com1_get_arg", [&](const std::string &requester_id)
+                                                                    { return [&](BaseObj a)
+                                                                      { const std::lock_guard<std::mutex> lock(arg_mutex); return arg; }; });
     }
 };
 
@@ -80,8 +78,9 @@ private:
 public:
     void operator()()
     {
-        //std::this_thread::sleep_for(std::chrono::seconds(randInt()));
-        if(com1_get_arg) {
+        // std::this_thread::sleep_for(std::chrono::seconds(randInt()));
+        if (com1_get_arg)
+        {
             auto a = com1_get_arg(BaseObj());
             std::cout << "Arg requested: a:" << a.a << ", b: " << a.d << std::endl;
         }
@@ -89,8 +88,36 @@ public:
 
     void submit_subscribe_and_requests_services(std::shared_ptr<rrps::IRequestServiceRegistration<BaseObj, BaseObj>> request_registrator)
     {
-        request_registrator->register_subscriber(id, "com1_arg_updated", [](BaseObj arg) { std::cout << "Arg updated: a:" << arg.a << ", b: " << arg.d << std::endl; } );
+        request_registrator->register_subscriber(id, "com1_arg_updated", [](BaseObj arg)
+                                                 { std::cout << "Arg updated: a:" << arg.a << ", b: " << arg.d << std::endl; });
 
         com1_get_arg = request_registrator->get_response_service(id, "com1_get_arg");
     }
 };
+
+int main(int argc, char const *argv[])
+{
+    auto rrps_service_mgr = std::make_shared<rrps::ReqResPubSubManager<BaseObj, BaseObj>>();
+    Com1 com1;
+    Com2 com2;
+    try
+    {
+        if (std::is_base_of<rrps::IServiceProvider<BaseObj, BaseObj>, Com1>::value)
+            com1.submit_publish_and_response_services(rrps_service_mgr);
+
+        if (std::is_base_of<rrps::IServiceRequester<BaseObj, BaseObj>, Com2>::value)
+            com2.submit_subscribe_and_requests_services(rrps_service_mgr);
+
+        for (auto i = 0; i < 5; ++i)
+        {
+            com1();
+            com2();
+        }
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return 0;
+}
