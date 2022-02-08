74530e2c10e3e24be28ef491a3e3673d1a94081c
Yashar A. Rezaei <yashar.a.rezaei@gmail.com>
2022-02-08 11:00:53 +0100

    Include guards updated to use hash

--- rrps/include/basic_types.hpp
+++ rrps/include/basic_types.hpp
@@ -1,22 +1,22 @@
-#ifndef RRPS_BASICE_TYPES_HPP_
-#define RRPS_BASICE_TYPES_HPP_
+#ifndef AA3A8B5D_9A0B_4C1E_B8C4_D4A5087348D1
+#define AA3A8B5D_9A0B_4C1E_B8C4_D4A5087348D1
 
 #include <string>
 #include <functional>
 
 namespace rrps
 {
-    template<class T_arg>
+    template <class T_arg>
     using SubCallback = std::function<void(T_arg)>;
-    
-    template<class T_arg>
-    using SubscribeServiceRegisterCallback = std::function<void(const std::string&, SubCallback<T_arg>)>;
-    
-    template<class T_return, class T_arg>
-    using ResponseCallback = std::function<T_return(T_arg)> ;
-    
-    template<class T_return, class T_arg>
-    using ResponseServiceRegisterCallback = std::function<ResponseCallback<T_return, T_arg>(const std::string&)>;
+
+    template <class T_arg>
+    using SubscribeServiceRegisterCallback = std::function<void(const std::string &, SubCallback<T_arg>)>;
+
+    template <class T_return, class T_arg>
+    using ResponseCallback = std::function<T_return(T_arg)>;
+
+    template <class T_return, class T_arg>
+    using ResponseServiceRegisterCallback = std::function<ResponseCallback<T_return, T_arg>(const std::string &)>;
 }
 
-#endif // end of RRPS_BASICE_TYPES_HPP_
+#endif /* AA3A8B5D_9A0B_4C1E_B8C4_D4A5087348D1 */
--- rrps/include/i_request_service_registration.hpp
+++ rrps/include/i_request_service_registration.hpp
@@ -1,5 +1,5 @@
-#ifndef RRPS_I_REQUEST_SERVICE_REGISTRATION_HPP_
-#define RRPS_I_REQUEST_SERVICE_REGISTRATION_HPP_
+#ifndef FF8826D8_7A03_4ACF_B524_3CB03B1E7982
+#define FF8826D8_7A03_4ACF_B524_3CB03B1E7982
 
 namespace rrps
 {
@@ -8,9 +8,9 @@ namespace rrps
     {
     public:
         virtual ~IRequestServiceRegistration() {}
-        virtual ResponseCallback<T_return, T_arg> get_response_service(const std::string& resquester_id, const std::string& response_service_id) = 0; 
-        virtual bool register_subscriber(const std::string& subscriber_id, const std::string& pub_service_id, SubCallback<T_arg> sub_callback) = 0;
+        virtual ResponseCallback<T_return, T_arg> get_response_service(const std::string &resquester_id, const std::string &response_service_id) = 0;
+        virtual bool register_subscriber(const std::string &subscriber_id, const std::string &pub_service_id, SubCallback<T_arg> sub_callback) = 0;
     };
 }
 
-#endif // end of RRPS_I_REQUEST_SERVICE_REGISTRATION_HPP_
+#endif /* FF8826D8_7A03_4ACF_B524_3CB03B1E7982 */
