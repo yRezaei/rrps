11ed79e381a7a71f7bc27adb1177dad2e8aa053c
Yashar A. Rezaei <yashar.a.rezaei@gmail.com>
2022-02-08 21:06:24 +0100

    Component dependency removed from IServiceProvider and IServiceRequester

--- rrps/include/i_request_service_registrator.hpp
+++ rrps/include/i_request_service_registrator.hpp
@@ -1,13 +1,16 @@
 #ifndef FF8826D8_7A03_4ACF_B524_3CB03B1E7982
 #define FF8826D8_7A03_4ACF_B524_3CB03B1E7982
 
+/*
+    rrps: Request-Respons & Publisher-Subscriber
+*/
 namespace rrps
 {
     template <class T_return, class T_arg>
-    class IRequestServiceRegistration
+    class IReceiveServiceRegistrator
     {
     public:
-        virtual ~IRequestServiceRegistration() {}
+        virtual ~IReceiveServiceRegistrator() {}
         virtual ResponseCallback<T_return, T_arg> get_response_service(const std::string &resquester_id, const std::string &response_service_id) = 0;
         virtual bool register_subscriber(const std::string &subscriber_id, const std::string &pub_service_id, SubCallback<T_arg> sub_callback) = 0;
     };
